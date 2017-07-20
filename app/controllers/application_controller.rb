bfd647376f9f9572cd0219f9436aee5268ad20bc
ken mcfadden <kendmcfadden@gmail.com>
2017-07-19 18:29:22 -0700

    initialize protect_from_forgery to null sesion to skip CSRF exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception
-  # km: not using CSRF token usint JWT..preclude exception if no CSRF
+  #protect_from_forgery with: :exception commented out for API approach.
+  # km: not using CSRF token using JWT..preclude exception if no CSRF
   protect_from_forgery with: :null_session
 
   respond_to :json
